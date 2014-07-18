47617ffa9f00406b00f337755c0bb8374e206fa6
Peter Walker <peter.walker78@gmail.com>
2014-07-18 07:27:05 +0100

    Added comments

--- wipiborg.py
+++ wipiborg.py
@@ -19,26 +19,29 @@ wiringpi.pinMode(PIN_RED, wiringpi.GPIO.OUTPUT)
 wiringpi.pinMode(PIN_GREEN, wiringpi.GPIO.OUTPUT)
 wiringpi.pinMode(PIN_BLUE, wiringpi.GPIO.OUTPUT)
 
-# Set global variable to control loop durations
+# Set global variable to control sub loop durations
 loopTime = "300"
 
 # Set up the colour map to allow simple colour selection when called
 colourMap = {"off":"000", "red":"100", "green":"010", "blue":"001", "yellow":"110", "cyan":"011", "magenta":"101", "white":"111"}
 
-# Logic to format passed in colour in the required way for SetLedBorg
+# Function to set the LED to the required colour
 def set_col(col):
 	colour = colourMap[col]
 #	print colour[0]
 #	print colour[1]
 #	print colour[2]
+# Logic to format passed in colour in the required way for SetLedBorg
 	RVAL = int(colour[0])
 	GVAL = int(colour[1])
 	BVAL = int(colour[2])
+# Logic to format passed in colour in the required way for SetLedBorg
 	wiringpi.digitalWrite(PIN_RED, RVAL)
 	wiringpi.digitalWrite(PIN_GREEN, GVAL)
 	wiringpi.digitalWrite(PIN_BLUE, BVAL)
 	print "the LED is " + str(col)
 
+# Class for checking number of unread emails
 class GmailAlert:
 	def __init__(self):
 		self.name = "Gmail Alert"
@@ -51,7 +54,8 @@ class GmailAlert:
 			return int(unreadCount) > int("5")
 		except:
 			print "Unable to retreive email stats"
-		
+
+# Class for check outside temperature		
 class WeatherAlert:
 	def __init__(self):
 		self.name = "Weather Alert"
@@ -66,7 +70,8 @@ class WeatherAlert:
 			return float(tempC) < float("4")
 		except:
 			print "Unable to retreive weather information"
-		
+
+# Class for checking for rail disruptions		
 class RailAlert:
 	def __init__(self):
 		self.name = "Rail Alert"
@@ -80,12 +85,14 @@ class RailAlert:
 		except:
 			print "Unable to retreive rail information"	 
 
+# Main handler to call classes and chuck active alerts into an array
 class AlertHandler:
 	def __init__(self):
 		self.alerts = []
 		self.gmail = GmailAlert()
 		self.weather = WeatherAlert()
 		self.rail = RailAlert()
+# Check the alerts
 	def active_alerts(self):
 		if not self.gmail in self.alerts:
 			if self.gmail.should_fire():
@@ -96,6 +103,7 @@ class AlertHandler:
 		if not self.rail in self.alerts:
 			if self.rail.should_fire():
 				self.alerts.append(self.rail)
+# Turn on the relevant LED colour
 	def set_led(self):
 		if self.gmail in self.alerts:
 			set_col("red")
@@ -106,21 +114,24 @@ class AlertHandler:
 		if self.rail in self.alerts:
 			set_col("magenta")
 			time.sleep(5)
-
+# Clear the alerts from the array for next check
 	def clear_alerts(self):
 		self.alerts = []
 		print self.alerts
-			
+
+# Main Programme loop			
 try:
 	while True:
 		ah = AlertHandler()
 		ah.active_alerts()
+# Work out the time to finish sub loops
 		finish_time = datetime.datetime.now() + datetime.timedelta(seconds=int(loopTime))
 		ft = str(finish_time)
-		
+# Code to execute if there are no alerts in the array		
 		if not ah.alerts:
 			print "No active alerts.. sleeping until " + ft[11:19]
 			while datetime.datetime.now() < finish_time:
+# Flashes the LED periodically to show that the programme has not hung
 				for x in range (0,5):
 					set_col("off")
 					time.sleep(0.05)
@@ -129,14 +140,18 @@ try:
 					set_col("off")
 				print "Still sleeping.. waking up at " + ft[11:19]
 				time.sleep(20)
+# Code to execute if there are alerts in the array
 		else:
 			print "Cycling through alerts until " + ft[11:19]
 			while datetime.datetime.now() < finish_time:
 				ah.set_led()
+# Clear the array for next pass
 		ah.clear_alerts()
 
+# Catch ctrl+c events
 except KeyboardInterrupt:
 	print "\nKeyboard interrupt detected. Quitting."
 
+# Turn off the LED before the programme quits
 finally:
 	set_col("off")
